324869ad5f1a8c96c77cb2c00a3df0fbe7b20735
Faisal-Althuwaini <faisal.yalthuwaini@gmail.com>
2025-10-02 15:07:32 +0300

    Changing user role functionality

--- backend/src/main/java/com/wakeb/jobsapplication/controller/UserController.java
+++ backend/src/main/java/com/wakeb/jobsapplication/controller/UserController.java
@@ -5,13 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.wakeb.jobsapplication.dto.UpdateRoleRequest;
 import com.wakeb.jobsapplication.dto.UserDTO;
@@ -45,7 +39,7 @@ class UserController {
         return ResponseEntity.ok().body("User deleted successfully");
     }
 
-    @PatchMapping("{email}/role")
+    @PutMapping("{email}/role")
     public ResponseEntity<UserDTO> updateUserRole(
             @PathVariable String email,
             @RequestBody UpdateRoleRequest updateRoleRequest) {
